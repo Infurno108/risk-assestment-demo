e7fd47888239932413047a51ca50ae677676fbcc
Flint Rose <oliverflintrose@gmail.com>
2023-05-13 17:37:11 -0500

    Ai passed every test without fail. Going to figure out how to upload the AI to a website.

--- src/dog.py
+++ src/dog.py
@@ -1,3 +1,10 @@
+# We need to start by making some synthetic data
+# We will need to have a few information per person, for this model to actually be able to show the concept I am going to squish the data to be smaller then it should be
+# [rent check per month (rent cost - payed), length of paying such rent(months), salery (gross income per month), current bank balance($$$$), savings ($$$), do you have a gig job?, credit score]
+# to create this data we will need to create a function that will create a list of lists
+# this function will create a spectrum of data between the lower bound and upper bound of each piece of data
+# this function will also create a list of labels for each piece of data, either 0 or 1, which will need to be created in a way that will make the data look like it is a real world example. Not to mention in the real test there has to be a way to acount for bias. No clue how.
+
 import numpy as np
 import keras as kr
 
@@ -6,16 +13,9 @@ percepitron = kr.models.Sequential()
 input = kr.layers.Dense(32, input_shape=[1])
 
 
-# We need to start by making some synthetic data
-# We will need to have a few information per person, for this model to actually be able to show the concept I am going to squish the data to be smaller then it should be
-# [rent check per month (rent cost - payed), length of paying such rent(months), salery (gross income per month), current bank balance($$$$), savings ($$$), do you have a gig job?, credit score]
-
 examplePiece = [-2, 12, 1000, 2400, 3000, 0, 500]
 anotherExamplePiece = [0, 6, 500, 4000, 1000, 1, 300]
 
-# to create this data we will need to create a function that will create a list of lists
-# this function will create a spectrum of data between the lower bound and upper bound of each piece of data
-# this function will also create a list of labels for each piece of data, either 0 or 1, which will need to be created in a way that will make the data look like it is a real world example. Not to mention in the real test there has to be a way to acount for bias. No clue how.
 
 exampleOne = np.array(examplePiece)
 exampleTwo = np.array(anotherExamplePiece)
--- src/puppy.py
+++ src/puppy.py
@@ -6,9 +6,11 @@ np.set_printoptions(suppress=True)
 
 # simple prototype of the prototype
 # the only inputs it will take are (monthly rent overflow (rent cost - rent payd), salery ($/month), current bank amount ($))
-
+# rent check per month (rent cost - payed), length of paying such rent(months), salery (gross income per month), current bank balance($$$$), savings ($$$), do you have a gig job?, credit score]
 
 # will create two lists, 1: [[rent, salery, bank], ...] 2: [yes,no,yes,yes,...] return both as a single list
+
+
 def createData():
     data = []
     labels = []
@@ -18,10 +20,14 @@ def createData():
         if (rent > 0):
             rent = 0
         salery = r.randint(1000, 8000)
-        bank = r.randint(0, 10000)
-        if (((bank + salery) + (rent * 20)) > 0):  # this is where human tuning is needed
+        bank = r.randint(0, 5000)
+        lengthofRent = r.randint(0, 24)
+        savings = r.randint(0, 10000)
+        gigJob = r.randint(0, 1)
+        # this is where human tuning is needed
+        if (((lengthofRent*rent) + (bank + salery + savings) + (rent * 20) + ((salery/2)*gigJob) > 0)):
             yesNo = 1
-        data.append([int(rent), int(salery), int(bank)])
+        data.append([rent, salery, bank, lengthofRent, savings, gigJob])
         labels.append(yesNo)
     data = np.array(data)
     labels = np.array(labels)
@@ -29,12 +35,10 @@ def createData():
     np.savetxt('people.csv', data, delimiter=',')
 
 
-# createData()
-
 riskyBusiness = kr.models.Sequential()
 # input layer of size 3 with a hidden layer of 16
-riskyBusiness.add(kr.layers.Dense(16, input_dim=3, activation='relu'))
-riskyBusiness.add(kr.layers.Dense(16, activation='relu'))
+riskyBusiness.add(kr.layers.Dense(30, input_dim=6, activation='relu'))
+riskyBusiness.add(kr.layers.Dense(30, activation='relu'))
 # output layer of size 1 to have a simple binary yes or no
 riskyBusiness.add(kr.layers.Dense(1))
 
@@ -45,19 +49,23 @@ riskyBusiness.compile(loss='mean_squared_error',
 data = np.loadtxt('people.csv', delimiter=',')
 labels = np.loadtxt('labels.csv', delimiter=',')
 
-# riskyBusiness.fit(data, labels, epochs=1000)
 
-# riskyBusiness.save('riskyBusiness.h5')
+def trainModel():
+    riskyBusiness.fit(data, labels, epochs=1000)
+    riskyBusiness.save('riskyBusiness.h5')
 
-runningModel = kr.models.load_model('riskyBusiness.h5')
 
-x = 29
-print(runningModel.predict(data[x].reshape(1, 3)))
-print(labels[x])
+def runModel():
+    runningModel = kr.models.load_model('riskyBusiness.h5')
+    for i in range(0, 25000):
+        print("prediction: ", runningModel.predict(
+            data[i].reshape(1, 6)), "actual: ", labels[i], "step: ", i)
+        totalFails = 0
+        if (int(runningModel.predict(data[i].reshape(1, 6))) != labels[i]):
+            totalFails += 1
+    print("fails: ", totalFails)
 
-for i in range(100, 400):
-    totalFails = 0
-    if (int(runningModel.predict(data[i].reshape(1, 3))) != labels[i]):
-        print(i)
-        totalFails += 1
-print("fails: ", totalFails)
+
+# createData()
+# trainModel()
+runModel()
