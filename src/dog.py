f6c810a70f76b80c49e74e5e8c143942a10de3fa
Flint Rose <oliverflintrose@gmail.com>
2023-05-15 13:48:23 -0500

    working ai, but the website never really ended up working. Oh well.

--- src/dog.py
+++ src/dog.py
@@ -1,18 +1,18 @@
 
 import numpy as np
 import keras as kr
-import tensorflowjs as tfjs
 import random as r
+import tensorflowjs as tfjs
 np.set_printoptions(suppress=True)
 
 # simple prototype of the prototype
-# the only inputs it will take are (monthly rent overflow (rent cost - rent payd), salery ($/month), current bank amount ($))
-# rent check per month (rent cost - payed), length of paying such rent(months), salery (gross income per month), current bank balance($$$$), savings ($$$), do you have a gig job?, credit score]
+# the only inputs it will take are (monthly rent overflow (rent cost - rent payd), salary ($/month), current bank amount ($))
+# rent check per month (rent cost - payed), length of paying such rent(months), salary (gross income per month), current bank balance($$$$), savings ($$$), do you have a gig job?, credit score]
 
-# will create two lists, 1: [[rent, salery, bank], ...] 2: [yes,no,yes,yes,...] return both as a single list
+# will create two lists, 1: [[rent, salary, bank], ...] 2: [yes,no,yes,yes,...] return both as a single list
 
 
-# rent, length, salery, bank, savings, gig,
+# rent, length, salary, bank, savings, gig,
 
 def createData():
     data = []
@@ -22,15 +22,15 @@ def createData():
         rent = r.randint(-1000, 500)
         if (rent > 0):
             rent = 0
-        salery = r.randint(1000, 8000)
+        salary = r.randint(1000, 8000)
         bank = r.randint(0, 5000)
         lengthofRent = r.randint(0, 24)
         savings = r.randint(0, 10000)
         gigJob = r.randint(0, 1)
         # this is where human tuning is needed
-        if (((lengthofRent*rent) + (bank + salery + savings) + (rent * 20) + ((salery/2)*gigJob) > 0)):
+        if (((lengthofRent*rent) + (bank + salary + savings) + (rent * 20) + ((salary/2)*gigJob) > 0)):
             yesNo = 1
-        data.append([rent, salery, bank, lengthofRent, savings, gigJob])
+        data.append([rent, salary, bank, lengthofRent, savings, gigJob])
         labels.append(yesNo)
     data = np.array(data)
     labels = np.array(labels)
@@ -61,7 +61,7 @@ def trainModel():
 
 def runModel():
     runningModel = kr.models.load_model('riskyBusiness.h5')
-    for i in range(0, 25000):
+    for i in range(0, 10):
         print("prediction: ", runningModel.predict(
             data[i].reshape(1, 6)), "actual: ", labels[i], "step: ", i)
         totalFails = 0
@@ -71,5 +71,6 @@ def runModel():
 
 
 # createData()
-trainModel()
+# trainModel()
 # runModel()
+runningModel = kr.models.load_model('riskyBusiness.h5')
